54d8e58c0826f279191940d76f783a45140324bd
mcgregor112 <ks8130624@gmail.com>
2024-08-10 23:24:54 +0530

    q1.c

--- assignments/se/module-3/q1.c
+++ assignments/se/module-3/q1.c
@@ -1,39 +1,51 @@
-// Display This Information using printf
-//1. Your Name
-//2. Your Birth date
-//3. Your Age
-//4. Your Address
+//q1.c
+
+//  Display This Information using printf
+// 1. Your Name
+// 2. Your Birth date
+// 3. Your Age
+// 4. Your Address
+
+//1. your name:
+
+#include <stdio.h>
+
+int main() {
+       
+        printf("krishna sharma" );
+    }
 
 
+
+//2. your birth date:
 
 #include<stdio.h>
-struct info
-{
 
-    char name;
-    char dob;
-    int age;
-    char address;
-};
-int main()
-{
-    struct info s;
-    printf("Enter your name:");
-    scanf("%s",&s.name);
-    printf("Enter your dob:");
-    scanf("%s",&s.dob);
-    printf("Enter your age:");
-    scanf("%d",&s.age);
-    printf("Enter your address:");
-    scanf("%s",&s.address);
+int main(){
 
-    printf("sname: %s\n",s.name);
-    printf("sname: %s\n",s.dob);
-    printf("sname: %d\n",s.age);
-    printf("sname: %s\n",s.address);
+    printf("18/06/2004" );
+}
 
-    return 0;
+//3. your age:
 
-    }
+#include<stdio.h>
+
+int main(){
+
+    int age = 20;
+    printf("%d",age);
+}
+
+//4. your addres:
+
+#include<stdio.h>
+
+int main(){
+
+     char address;
+         printf("Enter your address:",address);
+         scanf("%s",address);
+}
+
 
 
