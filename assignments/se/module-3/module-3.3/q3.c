63c63cd2cb7bae0487773760f75b36b910c5bff0
mcgregor112 <ks8130624@gmail.com>
2024-09-02 01:30:00 +0530

    module - 3.3

---
+++ assignments/se/module-3/module-3.3/q3.c
@@ -0,0 +1,32 @@
+// WAP to find reverse of string using recursion
+
+//Answer :
+
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char str[], int start, int end) {
+    char temp;
+    if (start >= end) {
+        return;
+    }
+    temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+    reverseString(str, start + 1, end - 1);
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    str[strcspn(str, "\n")] = '\0';
+
+    int length = strlen(str);
+
+    reverseString(str, 0, length - 1);
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
