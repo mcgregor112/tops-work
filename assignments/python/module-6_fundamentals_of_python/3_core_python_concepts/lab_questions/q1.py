a50072ed5b1bfaf82bbd8e78d4e3e473ee68c452
mcgregor112 <ks8130624@gmail.com>
2024-12-04 13:39:40 +0530

    module 6

---
+++ assignments/python/module-6_fundamentals_of_python/3_core_python_concepts/lab_questions/q1.py
@@ -0,0 +1,14 @@
+# q1 - Write a Python program to demonstrate the creation of variables and different data types.
+
+# Answer :
+
+name = str(input("Enter your name : "))
+age = int(input("Enter your age : "))
+
+print(name, age)
+
+# output :
+
+# Enter your name : krishna
+# Enter your age : 21
+# krishna 21
