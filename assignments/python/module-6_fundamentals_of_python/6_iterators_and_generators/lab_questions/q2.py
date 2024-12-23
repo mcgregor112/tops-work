3ab38292392337a8464b5113338ea98351d92f2f
mcgregor112 <ks8130624@gmail.com>
2024-12-23 11:32:04 +0530

    iterators and generators

--- assignments/python/module-6_fundamentals_of_python/6_iterators_and_generators/lab_questions/q2.py
+++ assignments/python/module-6_fundamentals_of_python/6_iterators_and_generators/lab_questions/q2.py
@@ -2,7 +2,17 @@
 
 # Answer :
 
+num = [1,2,3,4,5,6,7,8,9,10]
 
+n = iter(num)
+
+print(next(n))
+print(next(n))
+print(next(n))
+print(next(n))
+print(next(n))
+print(next(n))
+print(next(n))
 
 
 
