a20ab18f741f0853e2f307d865bad43221c16240
mcgregor112 <ks8130624@gmail.com>
2025-02-25 12:09:15 +0530

    prctice

---
+++ practice/practice.py
@@ -0,0 +1,68 @@
+# Create a tuple: Create a tuple with 5 elements and print the first, third, and last elements.
+
+# Answer :
+
+# name = (1,2,3,4,5)
+# print(name[0])
+# print(name[2])
+# print(name[4])
+
+# output :
+# 1
+# 3
+# 5
+
+# Concatenate tuples: Given two tuples, concatenate them and print the result.
+
+# Answer :
+
+# num = (6,7,8,9,10)
+# result = name + num
+# print(result)
+
+# output :
+
+# (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+# Check if element exists: Check if an element exists in a tuple.
+
+# Answer :
+
+# num = (6,7,8,9,10)
+
+# element = int(input("Enter the element for check :"))
+
+# if element in num :
+#     print("this elemnet exists in tuple")
+# else :
+#     print("this element not exists in tuple")    
+
+# output :
+
+# Enter the element for check :1
+# this element not exists in tuple
+
+# Enter the element for check :9
+# this elemnet exists in tuple
+
+# Tuple to list: Convert a tuple to a list and modify an element in the list
+
+# Answer :
+
+# num = (6,7,8,9,10)
+# print(list(num))
+
+# output :
+# [6, 7, 8, 9, 10]
+
+# Tuple slicing: Create a tuple with numbers from 1 to 10, slice it to return elements from index 3 to 7.
+
+# Answer :
+
+# fruits = (1,2,3,4,5,6,7,8,9,10)
+
+# print(fruits[2:7])
+
+# output :
+
+# (3, 4, 5, 6, 7)
